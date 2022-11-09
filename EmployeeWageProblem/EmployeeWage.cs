97f79ed2dcbd22031c31be04edce17ab85e3cdb4
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-10 04:52:10 +0530

    [Mahesh] Add: Employee Wage Using Switch Case

--- EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -8,7 +8,7 @@ namespace EmployeeWageProblem
 {
     internal class EmployeeWage
     {
-        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, PART_TIME_HR = 4, IS_PRESENT = 0, IS_FULL_TIME = 1, IS_PART_TIME = 2;
+        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, HALF_DAY_HR = 4, IS_PRESENT = 0, IS_FULL_TIME = 1, IS_HALF_TIME = 2;
         Random random = new Random();
         public void Attendance()
         {
@@ -21,13 +21,19 @@ namespace EmployeeWageProblem
         public void EmpWage()
         {
             int dailyEmpWage = 0, empHrs = 0;
-            int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULL_TIME)
-                empHrs = FULL_DAY_HR;
-            else if (empCheck == IS_PART_TIME)
-                empHrs = PART_TIME_HR;
-            else
-                empHrs = 0;
+            int empCheck = random.Next(0, 3); //0,1,2
+            switch (empCheck)
+            {
+                case IS_FULL_TIME:
+                    empHrs = FULL_DAY_HR;
+                    break;
+                case IS_HALF_TIME:
+                    empHrs = HALF_DAY_HR;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
             dailyEmpWage = WAGE_PER_HR * empHrs;
             Console.WriteLine("Employee Wage " + dailyEmpWage);
         }
