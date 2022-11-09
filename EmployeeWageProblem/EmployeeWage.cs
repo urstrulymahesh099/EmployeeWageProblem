2c0b7c39059c418fba9732dd96a5b8fc60289235
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-10 04:30:57 +0530

    [Mahesh]Add: Employee Attendence

---
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProblem
+{
+    internal class EmployeeWage
+    {
+        public void Attendance()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 0)
+                Console.WriteLine("Emplyoee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+
+    }
+}
---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage problem statement ");
+            EmployeeWage employee = new EmployeeWage();
+            employee.Attendance();
+        }
+    }
+}
