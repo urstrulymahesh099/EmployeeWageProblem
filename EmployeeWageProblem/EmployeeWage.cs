c9b1473de4b085d1b99d4461f1ce0878071c152b
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-10 05:07:52 +0530

    [Mahesh] Add: Employee Wage Condition

--- EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -8,7 +8,6 @@ namespace EmployeeWageProblem
 {
     internal class EmployeeWage
     {
-
         const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, HALF_DAY_HR = 4, IS_PRESENT = 0, IS_FULL_TIME = 1, IS_HALF_TIME = 2, TOTAL_WORKING_DAYS = 20;
         Random random = new Random();
         public void Attendance()
@@ -23,23 +22,25 @@ namespace EmployeeWageProblem
         {
             int totalEmpWage = 0, empHrs = 0;
             for (int i = 0; i < TOTAL_WORKING_DAYS; i++)
-            {
-                int empCheck = random.Next(0, 3); //0,1,2
-                switch (empCheck)
+                for (int j= 0; j < TOTAL_WORKING_DAYS && empHrs < 100; j++)
                 {
-                    case IS_FULL_TIME:
-                        empHrs += FULL_DAY_HR;
-                        break;
-                    case IS_HALF_TIME:
-                        empHrs += HALF_DAY_HR;
-                        break;
-                    default:
-                        empHrs += 0;
-                        break;
+                    int empCheck = random.Next(0, 3); //0,1,2
+                    switch (empCheck)
+                    {
+                        case IS_FULL_TIME:
+                            empHrs += FULL_DAY_HR;
+                            break;
+                        case IS_HALF_TIME:
+                            empHrs += HALF_DAY_HR;
+                            break;
+                        default:
+                            empHrs += 0;
+                            break;
+                    }
                 }
-            }
             totalEmpWage = WAGE_PER_HR * empHrs;
             Console.WriteLine("Total Employee Wage " + totalEmpWage);
+            Console.WriteLine("Total emp wage" + totalEmpWage);
         }
     }
 }
