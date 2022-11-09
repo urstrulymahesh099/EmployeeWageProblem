55664fcf92f23e2d5424945f0e794a490bfc3b3b
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-10 04:47:38 +0530

    [Mahesh] Add: EMP Part time wage

--- EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -8,7 +8,7 @@ namespace EmployeeWageProblem
 {
     internal class EmployeeWage
     {
-        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, IS_PRESENT = 0, IS_FULL_TIME = 1;
+        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, PART_TIME_HR = 4, IS_PRESENT = 0, IS_FULL_TIME = 1, IS_PART_TIME = 2;
         Random random = new Random();
         public void Attendance()
         {
@@ -21,13 +21,15 @@ namespace EmployeeWageProblem
         public void EmpWage()
         {
             int dailyEmpWage = 0, empHrs = 0;
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
                 empHrs = FULL_DAY_HR;
+            else if (empCheck == IS_PART_TIME)
+                empHrs = PART_TIME_HR;
             else
                 empHrs = 0;
             dailyEmpWage = WAGE_PER_HR * empHrs;
-            Console.WriteLine("Daily Employee Wage" + dailyEmpWage);
+            Console.WriteLine("Employee Wage " + dailyEmpWage);
         }
     }
 }
