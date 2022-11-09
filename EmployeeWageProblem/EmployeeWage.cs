c94bd40077f65ccf240beb4b6f227f10d437d44f
mago mahi <maheshhonnamurthy099@gmail.com>
2022-11-10 04:56:59 +0530

    [Mahesh] Add: Emp monthly wage

--- EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWage.cs
@@ -8,7 +8,8 @@ namespace EmployeeWageProblem
 {
     internal class EmployeeWage
     {
-        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, HALF_DAY_HR = 4, IS_PRESENT = 0, IS_FULL_TIME = 1, IS_HALF_TIME = 2;
+
+        const int WAGE_PER_HR = 20, FULL_DAY_HR = 8, HALF_DAY_HR = 4, IS_PRESENT = 0, IS_FULL_TIME = 1, IS_HALF_TIME = 2, TOTAL_WORKING_DAYS = 20;
         Random random = new Random();
         public void Attendance()
         {
@@ -20,22 +21,25 @@ namespace EmployeeWageProblem
         }
         public void EmpWage()
         {
-            int dailyEmpWage = 0, empHrs = 0;
-            int empCheck = random.Next(0, 3); //0,1,2
-            switch (empCheck)
+            int totalEmpWage = 0, empHrs = 0;
+            for (int i = 0; i < TOTAL_WORKING_DAYS; i++)
             {
-                case IS_FULL_TIME:
-                    empHrs = FULL_DAY_HR;
-                    break;
-                case IS_HALF_TIME:
-                    empHrs = HALF_DAY_HR;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                int empCheck = random.Next(0, 3); //0,1,2
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs += FULL_DAY_HR;
+                        break;
+                    case IS_HALF_TIME:
+                        empHrs += HALF_DAY_HR;
+                        break;
+                    default:
+                        empHrs += 0;
+                        break;
+                }
             }
-            dailyEmpWage = WAGE_PER_HR * empHrs;
-            Console.WriteLine("Employee Wage " + dailyEmpWage);
+            totalEmpWage = WAGE_PER_HR * empHrs;
+            Console.WriteLine("Total Employee Wage " + totalEmpWage);
         }
     }
 }
